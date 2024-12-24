70d7b3e17be9949c8fbced5e5e992ff7251f2392
Dhamy <damianllorenteguti@gmail.com>
2024-12-24 12:58:48 +0100

    player animations and included on code game v0.1.1

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,21 +10,59 @@ public class Player : MonoBehaviour
     private float inputH;
     [SerializeField] private float speedMovement;
     [SerializeField] private float jumpForce;
-    void Start()
+    private Animator animator;
+    private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
+    {
+        Move();
+        Attack();
+        Jump();
+    
+    }
+
+    private void Move()
     {
         inputH = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(inputH * speedMovement, rb.velocity.y) ;
+        rb.velocity = new Vector2(inputH * speedMovement, rb.velocity.y);
 
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (inputH > 0)
         {
-            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            animator.SetBool("isMoving", true);
+            transform.SetPositionAndRotation(transform.position, new Quaternion(0,0,0,1));
+        }
+        else if(inputH < 0)
+        {
+            transform.SetPositionAndRotation(transform.position, new Quaternion(0, -180, 0, 1));
+            animator.SetBool("isMoving", true);
+        }
+        else
+        {
+            animator.SetBool("isMoving", false);
+            
         }
 
+    }
+
+    private void Attack()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            animator.SetTrigger("attack");
+        }
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            animator.SetTrigger("jump");
+        }
     }
 }
